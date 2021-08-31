729c65b60c631262aeed468dea2f81b99c1c65a0
Haykrissha Cuezon <haykrissha.cuezon@lsu.edu.ph>
2021-08-31 23:43:08 +0800

    initial commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#1C1C1C");
+final Color accentHexColor = HexColor("#E4455E");
+final Color pinkHexColor = HexColor("#F2A2AF");
+final Color lightHexColor = HexColor("#F7C7CF");
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bmi_calculator_app1/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
